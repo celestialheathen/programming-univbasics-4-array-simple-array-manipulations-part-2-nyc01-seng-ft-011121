45432eb1785c19935664c0e5d81858dc70a9ddd9
Jingchao Yu <jingchaoyu@hotmail.com>
2021-01-01 19:24:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,20 +6,8 @@ def using_insert(array, element)
   array.insert(4, element)
 end
 
-describe "using_insert" do 
-  before(:each) do 
-    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    @another_language = "Python"
-    @new_array = using_insert(@list_of_programming_languages, @another_language)
-  end
-
-  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_language)
-  end
-
-  it "increases the length of the array" do 
-    expect(@new_array.length).to eq(8)
-  end
+def using_uniq(arr)
+  arr.uniq()
 end
 
 describe "using_uniq" do 
